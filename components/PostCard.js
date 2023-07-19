c3beb7cbee3546394385d4f51c5108c2ff009f49
maximoaprilesplice <108291030+maximoaprilesplice@users.noreply.github.com>
2023-07-19 12:31:26 -0300

    finished course

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,11 +1,9 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default function PostCard ({ post }){
-    return (
-        <Link href={post.uri} className={"card"}>
-            <a className="card">
-                <h3>{post.title} &rarr;</h3>
-            </a>
-        </Link>
-    )
+export default function PostCard({ post }) {
+  return (
+    <Link className="card" href={post.uri}>
+      <h3>{post.title} &rarr;</h3>
+    </Link>
+  );
 }
